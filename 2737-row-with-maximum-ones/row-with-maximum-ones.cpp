c2e520ff3b6dc073655c5014058119fa23f3e4ae
robitsi <robitkumar4g@gmail.com>
2025-10-14 21:52:16 +0530

    Time: 2 ms (67.83%) | Memory: 64.6 MB (90.24%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -4,10 +4,10 @@ public:
         int maxOnes = 0;
         int rowIndex = 0;
 
-        for (int i = 0; i < mat.size(); i++) {
+        for (int i = 0; i < mat.size(); ++i) {
             int count = 0;
             for (int val : mat[i]) {
-                count += val; 
+                count += val; // since values are 0 or 1
             }
 
             if (count > maxOnes) {
